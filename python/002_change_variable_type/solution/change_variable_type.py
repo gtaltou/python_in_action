5eb7da35311df785cebc287771cc13e7955b18bd
Guy Taltou <guy.taltou@gmail.com>
2021-12-12 22:07:07 +0100

    change of variable type

---
+++ python/002_change_variable_type/solution/change_variable_type.py
@@ -0,0 +1,4 @@
+a = 20
+b = input("Enter a number: ")
+result = a + int(b)
+print("The result of the operation is " + str(result))
---
+++ python/002_change_variable_type/source/change_variable_type.py
@@ -0,0 +1,5 @@
+# Ask the user to enter a number
+# Display the result of the addition of this number with the number a.
+# For example: "The result of the operation is 30" (in case the user enters the number 10)
+
+a = 20
