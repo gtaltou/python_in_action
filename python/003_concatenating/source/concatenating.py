d6aad65c894b510bb9eaf8cc29c3187dde223a71
Guy Taltou <guy.taltou@gmail.com>
2021-12-12 22:21:35 +0100

    Concatenating

---
+++ python/003_concatenating/source/concatenating.py
@@ -0,0 +1,17 @@
+"""
+In this exercise, you must use the conversion functions
+to convert the objects to the right type in order to display the following sentences
+and avoid errors.
+Sentences to display:
+I have a class of 30 students
+10 + 5 is equal to 15
+15
+The addition of 10 + 5 is equal to 15
+"""
+
+a = "I have a class of " + 30 + " students"
+#b = 10 + " + "5" + " is equal to " + 15
+c = 10 + "5"
+d = "The addition of 10 + 5 is equal to " + 10 + 5
+
+#Translated with www.DeepL.com/Translator (free version)
