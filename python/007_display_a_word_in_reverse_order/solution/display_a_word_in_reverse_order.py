3ea7f01464eb1b1bfe5e0fbe882cf0af9075fe15
Guy Taltou <guy.taltou@gmail.com>
2022-01-25 16:07:58 +0100

    Displaying a word in reverse order

---
+++ python/007_display_a_word_in_reverse_order/solution/display_a_word_in_reverse_order.py
@@ -0,0 +1,9 @@
+# Solution 1
+word = "Python"
+for lettre in reversed(word):
+    print(lettre)
+
+# Solution 2
+word = "Python"
+for lettre in word[::-1]:
+    print(lettre)
---
+++ python/007_display_a_word_in_reverse_order/source/display_a_word_in_reverse_order.py
@@ -0,0 +1,10 @@
+"""
+In this exercise you need to display the letters of the word 'Python' in reverse order.
+Your script should therefore display :
+n
+o
+h
+t
+y
+P
+"""
